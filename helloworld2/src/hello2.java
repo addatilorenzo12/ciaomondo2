0bf031d9f799642b013024c36b29b9a7852d033d
knoppix <knoppix@Microknoppix>
2015-04-11 11:30:02 +0200

    hellos2

---
+++ helloworld2/src/hello2.java
@@ -0,0 +1,14 @@
+
+public class hello2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("addati lorenzo");
+		System.out.println("11-4-2015");
+		System.out.println("Hello world!");
+
+	}
+
+}
