ab6a3b0068fa38ec255213af5b4e77053efc89fa
knoppix <knoppix@Microknoppix>
2015-04-11 11:41:46 +0200

    secondo

--- helloworld2/src/hello2.java
+++ helloworld2/src/hello2.java
@@ -5,7 +5,7 @@ public class hello2 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("addati lorenzo");
+		System.out.println(" the bbest addati lorenzo");
 		System.out.println("11-4-2015");
 		System.out.println("Hello world!");
 
